4003017d4363c08afe9897df322d0b33fe841c96
Michael Qian <michaelymq@gmail.com>
2022-05-13 02:41:38 -0400

    implement update student (PUT)

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -36,5 +38,25 @@ public class StudentService {
             throw new IllegalStateException("student with id " + studentId);
         }
         studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId).
+                orElseThrow(() -> new IllegalStateException("student with id " + studentId + " does not exist"));
+
+        // change name of existing student
+        if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)) {
+            student.setName(name);
+        }
+
+        if (email != null && email.length() > 0 &&
+                !Objects.equals(student.getEmail(), email)) {
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+            if (studentOptional.isPresent()) {
+                throw new IllegalStateException("email is taken");
+            }
+            student.setEmail(email);
+        }
     }
 }
